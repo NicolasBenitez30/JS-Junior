7aa0439b32f263d9e8f62e3f2d7638a0f089311f
NicolasBenitez30 <83706403+NicolasBenitez30@users.noreply.github.com>
2026-07-12 17:42:21 -0300

    6:21:27

--- Metodos de Cadenas/codigo.js
+++ Metodos de Cadenas/codigo.js
@@ -24,7 +24,7 @@ document.write(resultado5); // 18
 let resultado6 = cadena2.padStart(20, "8avos");
 document.write(resultado6); // 8avos8avos8avocadena
 // VA RELLENANDO PERO AL CONTRARIO QUE LA ANTERIOR, RELLENA EL FINAL DE LA CADENA. DESPUES ES EXACTAMENTE IGUAL.
-let resultado7 = cadena2.padEnds(10, "8avos")
+let resultado7 = cadena2.padEnd(10, "8avos");
 document.write(resultado7); // cadena8avo 
 // DEVUELVE LA MISMA CADENA PERO LA CANTIDAD QUE QUERRAMOS.
 let resultado8 = cadena2.repeat(3);
@@ -55,4 +55,97 @@ let resultado15 = cadena8.trimEnd();
 document.write(resultado15.length); // 11
 // ELIMINA TODOS LOS ESPACIOS DEL INICIO
 let resultado16 = cadena8.trimStart();
-document.write(resultado16.length); // 12
+document.write(resultado16.length + "<br><br>"); // 12
+
+
+//      ------ METODOS DE ARRAYS ------
+//          --- TRANSFORMADORES ---
+let nombres = ["pedro", "maria", "jorge"];
+document.write("Array Original: <b>" + nombres + "</b><br>");
+//  POP: Muestra y Saca el ultimo Elemento de la lista(Array).
+let resultadoPop = nombres.pop();
+document.write("Elemento Removido: <b style='color:red'>" + resultadoPop.toUpperCase() + "</b><br>");
+document.write("Resultado: <b>" + nombres + "</b><br><br>");
+//  SHIFT: Remueve el primero elemento de la lista y lo devuelve.
+let nombres2 = ["pedro", "maria", "jorge"];
+document.write("Array Original: <b>" + nombres2 + "</b><br>");
+let resultadoShift = nombres2.shift(); 
+document.write("Elemento Removido: <b style='color:red'>" + resultadoShift.toUpperCase() + "</b><br>");
+document.write("Resultado: <b>" + nombres2 + "</b><br><br>");
+//  PUSH: Agrega elementos al final de la lista.
+document.write("Array Original: <b>" + nombres + "</b><br>");
+nombres.push("juancito", "julian");
+document.write("Resultado: <b>" + nombres + "</b><br><br>");
+// REVERSE: Devuelve el Array pero alreves.
+let numeros = [1,2,3,4,5];
+document.write(`Array Original: <b>${numeros}</b><br>`);
+numeros.reverse();
+document.write(`Reverse: <b>${numeros}</b><br><br>`);
+//  UNSHIFT: Agrega elementos al principio de la lista.
+document.write(`Array: <b>${numeros}</b><br>`);
+numeros.unshift(0,9,8,7,6);
+document.write(`UnShift: <b>${numeros}</b><br><br>`);
+// SORT: Ordena los elementos de la lista de manera alfabetica lexico grafico.
+document.write(`Array: <b>${numeros}</b><br>`);
+numeros.sort();
+document.write(`Sort: <b>${numeros}</b><br>`);
+document.write("Nombres: <b>" + nombres + "</b><br>");
+nombres.sort();
+document.write("Nombres Sort: <b>" + nombres + "</b><br><br>");
+// SPLICE: Elimina elementos de la lista segun la posicion y la cantidad que le indiquemos, tambien podemos reemplazar esos elementos indicandolo despues de las otras dos condiciones, tambien se puede solamente agregar elementos, indicando la posicion, indicando 0 elementos a eliminar y luego indicar los elementos a agregar al array.
+document.write("Nombres: <b>" + nombres + "</b><br>");
+nombres.splice(1,1, "julian", "enzo", "lionel");
+document.write("Nombres Splice: <b>" + nombres + "</b><br><br>");
+
+//      --- ACCESORES ---
+//  JOIN: Nos separa los elementos de la lista de distinta manera
+document.write("Nombres: <b>" + nombres + "</b><br>");
+let resultadoJoin = nombres.join(" - ");
+document.write("Resultado: <b>" + resultadoJoin + "</b><br><br>");
+//  SLICE: Nos muestra los elementos a partir de la posicion y la cantidad
+document.write("Nombres: <b>" + nombres + "</b><br>");
+let resultadoSlice = nombres.slice(0, 2);// si queremos ver todos, usamos -1 en lugar de la cantidad o en este caso 2.
+document.write("Resultado: <b>" + resultadoSlice + "</b><br><br>");
+
+
+//          --- DE REPETECCION ---
+//  FILTER: Crea un nuevo array con todos los elementos que cumplan la condicion.
+document.write("Array: <b>" + nombres + "</b><br>");
+let nombre;
+let resultadoFilter = nombres.filter(nombre => nombre.length > 5);
+document.write("Resultado: <b>" + resultadoFilter + "</b><br><br>");
+
+
+
+//      ------ OBJETO MATH: BASICO ------
+//          --- METODOS ---
+//  SQRT: Devuelve la Raiz cuadrada del numero que indiquemos.
+let numeroSQRT = Math.sqrt(25);
+document.write(`Resultado Raiz Cuadrada de 25: <b>${numeroSQRT}</b><br>`);
+// CBRT: Devuelve la Raiz Cubica del numero que indiquemos.
+let numeroCBRT = Math.cbrt(27);
+document.write(`Resultado Raiz cubica de 27: <b>${numeroCBRT}</b><br><br>`);
+//  MAX: Devuelve el numero mas grande de la lista que le asignamos.
+document.write(`Lista de numeros: <b>12,300,33,912,25,543</b><br>`);
+let numeroMAX = Math.max(12,300,33,912,25,543);
+document.write(`El Mas Grande: <b>${numeroMAX}</b><br><br>`);
+//  MIN: Devuelve el numero mas chico de la lista que le asignamos.
+document.write(`Lista de numeros: <b>12,300,33,912,25,543</b><br>`);
+let numeroMIN = Math.min(12,300,33,912,25,543);
+document.write(`El Mas CHICO: <b>${numeroMIN}</b><br><br>`);
+//  RANDOM: Nos devuelve un numeor random entre 0 y 1.
+let numeroRANDOM = Math.random()*100; // Si queremos que se entre 0 a 100 de esta manera lo logramos
+document.write(`Numero Random entre 0 a 100: <b>${numeroRANDOM}</b><br><br>`);
+// ROUND: Nos devuelve un numero entero redondeando mas cercano.
+let numeroROUND = Math.round(numeroRANDOM);
+document.write(`Numero Random entre 0 y 100 ENTERO: <b>${numeroROUND}</b><br><br>`);
+// FLOOR: Nos da un numero en decimales redondeando a Entero para Abajo.
+let numRAM2 = Math.random()*99
+let numeroFLOOR = Math.floor(numRAM2+1);
+document.write(`Numero Random entre 0 a 100 ENTERO: <b>${numeroFLOOR}</b><br><br>`);
+// FROUND: Nos devuelve un numero redondeado de 4 bits decimales.
+let numeroFROUND = Math.fround(7.777777712832213213432144212421);
+document.write(`Numero : <b>${numeroFROUND}</b><br><br>`);
+// TRUNC: Devuelve el numero sin sus decimales
+let numeroTRUNC = Math.trunc(5.9999);
+document.write(`Numero sin Decimales: <b>${numeroTRUNC}</b><br><br>`);
--- Metodos de Cadenas/readme.md
+++ Metodos de Cadenas/readme.md
@@ -22,3 +22,46 @@
 - valueOf(): Retorna el valor primitivo de un objeto string.
 
 # Metodos de Arrays
+
+Transformadores
+
+- POP: Elimina el ultimo elemento de un array y lo devuelve.
+- SHIFT: Elimina el primer elemento de un arrat y lo devuelve.
+- PUSH: Agrega un elemneto al arrat al final de la lista.
+- REVERSE: Invierte el orden de los elementos de un array.
+- UNSHIFT: Agrega uno o mas elementos al inicio del array, y devuelve la nueva longitud del array.
+- SORT: Ordena los elementos de unarreglo (array) localmente y devuelve el arreglo ordenado.
+- SPLICE: Cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
+
+Accesores
+- JOIN: Une todos los elementos de una matriz (u objeto similar) en una cadena y la devuelve.
+- SLICE: Devuelve una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido).
+- Metodos ya vistos en cadenas: toString(), indexOf(), lastIndexOf(), includes().
+
+DE REPETICION
+- FILTER: Crea un nuevo array con todos los elementos que cumplan la condicion.
+- FOREACH: Ejecuta la funcion indicanda una vez por cada elemento del array.
+
+# Objeto MATH - Basico
+
+METODOS
+- SQRT: Devuelve la raiz cuadrada positiva de un numero.
+- CBRT: Devuelve la raiz cubica de un numero.
+- MAX: Devuelve el mayor de cero o mas numeros.
+- MIN: Devuelve el mas pequeño de cero o mas numeros.
+- RANDOM: Devuelve un numero pseudo-aleatorio entre 0 y 1.
+- ROUND: Devuelve el valor de un numero redondeado al numero entero mas cercano.
+- FROUND: Devuelve la representacion flotante de precision simple mas cercana de un numero.
+- FLOOR: Devuvelve el mayor entero menor que o igual a un numero.
+- TRUNC: Devuvelve la parte entera del numero X, la eliminacion de los digitos fraccionarios.
+
+PROPIEDADES
+- PI: Ratio de la circunferencia de un circulo respecto a su diametro, aproximadamente 3.14159.
+- SQRT1_2: Raiz cuadrada de 1/2: Equivalente, 1 sobre la raiz cuadrada de 2, aproximadamente 0.707.
+- SQRT2: Raiz cuadrada de 2, aproximadamente 1.414.
+
+- E: Constante de Euler, la base de los logaritmos naturales, aproximadamente 2.718.
+- LN2: Logaritmo natural de 2, aproximadamente 0.693.
+- LN10: Logaritmo natural de 10, aproximadamente 2.303
+- LOG2E: Logaritmo de E con base 2, aproximadamente 1.443.
+- LOG10E: Logaritmo de E con base 10, aproximadamente 0.434.
